e93869cfb0c018e5188ec503cb24e8e4264a3898
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-10-24 19:22:39 -0300

    Update código

--- UT3/webworkers/src/main.js
+++ UT3/webworkers/src/main.js
@@ -1,7 +1,14 @@
-import './style.css'
+import './style.css';
+import {
+  naiveSubsetSum, randomSubsetSumProblem, testSubsetSum,
+} from './subset-sum';
 import {
   newWorker, echoWorker, tickWorker, workerFunction,
 } from './utils';
+
+window.naiveSubsetSum = naiveSubsetSum;
+window.randomSubsetSumProblem = randomSubsetSumProblem;
+window.testSubsetSum = testSubsetSum;
 
 window.newWorker = newWorker;
 window.echoWorker = echoWorker;
